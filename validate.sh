103e6effc02a09ae98e78668eaa6511c031a0040
Yomguithereal <guillaumeplique@gmail.com>
2020-11-25 20:45:42 +0100

    Upgrading to frictionless

--- validate.sh
+++ validate.sh
@@ -5,6 +5,5 @@ for corpus in $(cat corpora.txt)
 do
   echo "Validating '$corpus'..."
   pkg=$corpus/datapackage.json
-  datapackage validate $pkg
-  goodtables $pkg
+  frictionless validate $pkg
 done
